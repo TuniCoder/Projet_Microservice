186e12b1310d04f6ef2575352d44e08995296145
TuniCoder <sofiennemrabet16@gmail.com>
2025-03-28 17:56:46 +0100

    add product category service

---
+++ ProductsCategories/src/main/java/com/espirt/microservice/productscategories/restController/RestCategoryController.java
@@ -0,0 +1,35 @@
+package com.espirt.microservice.productscategories.restController;
+
+import com.espirt.microservice.productscategories.entity.Category;
+import com.espirt.microservice.productscategories.services.serv.ServiceCategoryImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/prod/Categories")
+public class RestCategoryController {
+    ServiceCategoryImpl serviceCategory;
+    @PostMapping("/")
+    public Category addCategory(@RequestBody Category category){
+        return serviceCategory.addCategory(category);
+    }
+    @PutMapping("/{id}")
+    public Category updateCategory(@RequestBody Category category , @PathVariable Long id){
+        return serviceCategory.updateCategory(category , id);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id){
+        serviceCategory.deleteCategory(id);
+    }
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        return serviceCategory.getCategoryById(id);
+    }
+    @GetMapping("/")
+    public List<Category> getAllCategories(){
+        return serviceCategory.getAllCategories();
+    }
+}
---
+++ ProductsCategories/src/main/java/com/espirt/microservice/productscategories/services/Interfaces/IserviceProduts.java
@@ -0,0 +1,18 @@
+package com.espirt.microservice.productscategories.services.Interfaces;
+
+import com.espirt.microservice.productscategories.entity.Products;
+
+import java.util.List;
+
+public interface IserviceProduts {
+
+    Products addProduct(Products product);
+    Products updateProduct(Products product, Long id);
+    void deleteProduct(Long id);
+    Products getProductById(Long id);
+    List<Products> getAllProducts();
+    List<Products> getProductsByCategory(Long id);
+
+
+
+}
