5696cee26d871849387a4c124c7cdb1dad4c3d92
TuniCoder <sofiennemrabet16@gmail.com>
2025-04-22 21:56:46 +0100

    update web config

--- ProductsCategories/src/main/java/com/espirt/microservice/productscategories/WebConfig.java
+++ ProductsCategories/src/main/java/com/espirt/microservice/productscategories/WebConfig.java
@@ -3,12 +3,14 @@ package com.espirt.microservice.productscategories;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 
 @Configuration
 @EnableWebMvc
+
 
 public class WebConfig implements WebMvcConfigurer {
 
